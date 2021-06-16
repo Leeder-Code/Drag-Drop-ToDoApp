a6ca2030e7741bf2600417167c7904aa40f0b662
Mati-Code <mati.leeder@gmail.com>
2021-06-16 21:10:08 +0200

    remove test function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,13 +52,3 @@ const Home: FC = () => {
   )
 }
 export default Home
-
-// export const getStaticProps = async () => {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
-//   const articles = await res.json()
-//   return {
-//     props: {
-//       articles,
-//     },
-//   }
-// }
